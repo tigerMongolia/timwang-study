cbb9817d3bde2ecfa5bd31bc381fa290adcc5753
wj89757 <961214034@qq.com>
2020-06-12 14:07:00 +0800

    add hashmap init

---
+++ timwang-sourcecode/src/main/java/tim/wang/sourcecode/collections/hashmap/HashMapUtil.java
@@ -0,0 +1,29 @@
+package tim.wang.sourcecode.collections.hashmap;
+
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangjun
+ * @date 2020-06-12
+ */
+public class HashMapUtil {
+
+    public static void main(String[] args) {
+        debugHashMapConstructMethod();
+        debugHashMapPutMethod();
+    }
+
+    private static void debugHashMapConstructMethod() {
+        // 只是设置加载因子0.75
+        Map map = new HashMap();
+    }
+
+    private static void debugHashMapPutMethod() {
+        Map map = new HashMap();
+        map.put("key", "value");
+    }
+
+}
---
+++ timwang-sourcecode/src/main/java/tim/wang/sourcecode/collections/hashmap/MyHashMap.java
@@ -0,0 +1,34 @@
+package tim.wang.sourcecode.collections.hashmap;
+
+import java.util.Map;
+
+/**
+ * @author wangjun
+ * @date 2020-06-12
+ */
+public class MyHashMap<K, V> implements MyMap<K, V> {
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    private static class MapEntry<K, V> {
+        // private
+    }
+}
