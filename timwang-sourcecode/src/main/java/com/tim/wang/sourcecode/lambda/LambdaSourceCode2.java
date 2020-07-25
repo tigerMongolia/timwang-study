f41549febf55526660537007d38830086bd7a530
wj89757 <961214034@qq.com>
2020-07-25 21:58:18 +0800

    关于Java Lambda表达式实现和原理以及如何应用“

---
+++ timwang-sourcecode/src/main/java/com/tim/wang/sourcecode/lambda/LambdaSourceCode2.java
@@ -0,0 +1,13 @@
+package com.tim.wang.sourcecode.lambda;
+
+/**
+ * @author wangjun
+ * @date 2020-07-25
+ */
+public class LambdaSourceCode2 {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            System.out.println("====");;
+        }).start();
+    }
+}
